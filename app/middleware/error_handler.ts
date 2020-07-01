363b4603561005104470a3369da33e49f6a2d179
Krislee <krislee@KrisleedeiMac.local>
2020-07-02 00:49:24 +0800

    fix: 规范问题更改

--- app/middleware/error_handler.ts
+++ app/middleware/error_handler.ts
@@ -1,16 +1,13 @@
-import {Context} from 'egg';
-
+import { Context } from "egg";
 
 module.exports = () => {
-    return async function errorHandler(ctx:Context, next) {
-      try {
-       
-        await next();
-        
-      } catch (err) {
-        console.log(err);
-        ctx.app.logger.error(err);
-        ctx.responseBizError(err);
-      }
-  }
-}
+  return async function errorHandler(ctx: Context, next) {
+    try {
+      await next();
+    } catch (err) {
+      console.log(err);
+      ctx.app.logger.error(err);
+      ctx.responseBizError(err);
+    }
+  };
+};
