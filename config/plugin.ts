ddea54bbd3e211dd8b80782a4f65f8bc67b4e7b2
OF3493 <575558044@qq.com>
2020-11-27 17:47:45 +0800

    fix：问题文件更改

--- config/plugin.ts
+++ config/plugin.ts
@@ -6,10 +6,6 @@ const plugin: EggPlugin = {
     enable: true,
     package: "egg-validate",
   },
-  cors: {
-    enable: true,
-    package: "egg-cors",
-  },
 };
 
 export default plugin;
--- typings/config/plugin.d.ts
+++ typings/config/plugin.d.ts
@@ -15,7 +15,6 @@ import 'egg-static';
 import 'egg-jsonp';
 import 'egg-view';
 import 'egg-validate';
-import 'egg-cors';
 import { EggPluginItem } from 'egg';
 declare module 'egg' {
   interface EggPlugin {
@@ -32,6 +31,5 @@ declare module 'egg' {
     jsonp?: EggPluginItem;
     view?: EggPluginItem;
     validate?: EggPluginItem;
-    cors?: EggPluginItem;
   }
 }
