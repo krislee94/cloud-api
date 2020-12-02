8276b4fe5aa10156e20ed90baa23523ab887f584
os-krislee <575558044@qq.com>
2020-12-02 16:56:33 +0800

    fix；11

--- config/config.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => {
-    const config = {};
-    //   config.middleware = ["errorHandler"];
-    return Object.assign({}, config);
-};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLnRlc3QuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJjb25maWcudGVzdC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUVBLGtCQUFlLEdBQUcsRUFBRTtJQUNsQixNQUFNLE1BQU0sR0FBRyxFQUFtQixDQUFDO0lBQ25DLDBDQUEwQztJQUUxQyx5QkFDSyxNQUFNLEVBQ1Q7QUFDSixDQUFDLENBQUMifQ==
